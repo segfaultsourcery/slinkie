59dbf339773f991d66cfeed0358d27e0a1f6cfa8
Kim Hermansson <segfaultsourcery@users.noreply.github.com>
2017-07-17 17:25:53 +0200

    - bumped version number.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='slinkie',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='A LINQ-esque project.',
     long_description=long_description,
