3ee0acc0a73720fc02b837e741170d9308eb9725
Kim Hermansson <segfaultsourcery@users.noreply.github.com>
2017-07-20 09:26:34 +0200

    - Rearranged and categorized functions. - Removed unused aliases.

--- slinkie.py
+++ slinkie.py
@@ -17,6 +17,16 @@ class Slinkie:
     def __next__(self):
         return next(self._items)
 
+    def between(self, a, b, key=None):
+        """
+        Returns items between a and b. (Inclusive).
+        """
+
+        if key is None:
+            return Slinkie(filter(lambda it: a <= it <= b, self._items))
+
+        return Slinkie(filter(lambda it: a <= key(it) <= b, self._items))
+
     def consume(self, n=None):
         """
         Consume n items. If n is None, consume everything.
@@ -29,52 +39,32 @@ class Slinkie:
             pass
         return self
 
+    def exclude(self, items, key=None):
+        """
+        Excludes all items based on either their identity, or a key function.
+        """
+        if key:
+            keys = list(map(key, items))
+            return Slinkie(filter(lambda it: key(it) not in keys, self._items))
+
+        return Slinkie(filter(lambda it: it not in items, self._items))
+
+    def extend(self, items):
+        """
+        Yields all the items from this._items, followed by the items supplied to this function.
+        """
+
+        def _inner():
+            yield from self
+            yield from iter(items)
+
+        return Slinkie(_inner())
+
     def filter(self, key):
         """
         Filter the items.
         """
         return Slinkie(filter(key, self._items))
-
-    def map(self, transform, with_index=False, with_previous=False):
-        """
-        Map the items.
-        """
-
-        items = enumerate(self._items) if with_index else self._items
-
-        if with_previous:
-            # The transform function should accept two arguments, the previous and current items.
-            def inner():
-                previous = (None, None) if with_index else None
-                for item in items:
-                    yield previous, item
-                    previous = item
-
-            return Slinkie(transform(previous, item) for previous, item in inner())
-
-        return Slinkie(map(transform, items))
-
-    def skip(self, n):
-        """
-        Skip n items.
-        """
-        for i in range(n):
-            next(self._items)
-        return self
-
-    def take(self, n):
-        """
-        Take n items.
-        """
-
-        def inner():
-            try:
-                for _ in range(n):
-                    yield next(self._items)
-            except StopIteration:
-                return
-
-        return Slinkie(inner())
 
     def first(self, key=None):
         """
@@ -89,6 +79,23 @@ class Slinkie:
         If there are no objects, None is returned.
         """
         return next(self, None) if key is None else next(filter(key, self._items), None)
+
+    def flatten(self):
+        """
+        Flatten a two-dimensional result set into a single dimension.
+        """
+        return Slinkie(chain.from_iterable(self._items))
+
+    def group(self, key):
+        """
+        Groups all items on key.
+        """
+        grouped = defaultdict(list)
+
+        for it in self._items:
+            grouped[key(it)].append(it)
+
+        return Slinkie((k, Slinkie(v)) for k, v in grouped.items())
 
     def last(self, key=None):
         """
@@ -110,79 +117,30 @@ class Slinkie:
         except StopIteration:
             return None
 
-    def flatten(self):
+    def map(self, transform, with_index=False, with_previous=False):
         """
-        Flatten a two-dimensional result set into a single dimension.
-        """
-        return Slinkie(chain.from_iterable(self._items))
-
-    def between(self, a, b, key=None):
-        """
-        Returns items between a and b. (Inclusive).
+        Map the items.
         """
 
-        if key is None:
-            return Slinkie(filter(lambda it: a <= it <= b, self._items))
+        items = enumerate(self._items) if with_index else self._items
 
-        return Slinkie(filter(lambda it: a <= key(it) <= b, self._items))
+        if with_previous:
+            # The transform function should accept two arguments, the previous and current items.
+            def inner():
+                previous = (None, None) if with_index else None
+                for item in items:
+                    yield previous, item
+                    previous = item
+
+            return Slinkie(transform(previous, item) for previous, item in inner())
+
+        return Slinkie(map(transform, items))
 
     def not_none(self):
         """
         Returns all items except None.
         """
         return Slinkie(filter(lambda it: it is not None, self._items))
-
-    def group(self, key):
-        """
-        Groups all items on key. 
-        """
-        grouped = defaultdict(list)
-
-        for it in self._items:
-            grouped[key(it)].append(it)
-
-        return Slinkie((k, Slinkie(v)) for k, v in grouped.items())
-
-    def extend(self, items):
-        """
-        Yields all the items from this._items, followed by the items supplied to this function.
-        """
-
-        def _inner():
-            yield from self
-            yield from iter(items)
-
-        return Slinkie(_inner())
-
-    def exclude(self, items, key=None):
-        """
-        Excludes all items based on either their identity, or a key function.
-        """
-        if key:
-            keys = list(map(key, items))
-            return Slinkie(filter(lambda it: key(it) not in keys, self._items))
-
-        return Slinkie(filter(lambda it: it not in items, self._items))
-
-    def partition(self, n):
-        """
-        Takes n items and returns them in a new Slinkie. Does so until the items are consumed.
-        """
-
-        def inner():
-            while True:
-                result = self.take(n).list()
-                if not result:
-                    return
-                yield Slinkie(result)
-
-        return Slinkie(inner())
-
-    def sort(self, key=None, reverse=False):
-        """
-        Sorts the items by key.
-        """
-        return Slinkie(sorted(self._items, key=key, reverse=reverse))
 
     def parallelize(self, fn, threads=8):
         """
@@ -200,17 +158,47 @@ class Slinkie:
 
         return Slinkie(inner())
 
-    def join(self, glue=''):
+    def partition(self, n):
         """
-        Joins the items by glue, where glue is a string. Calls glue.join.
+        Takes n items and returns them in a new Slinkie. Does so until the items are consumed.
         """
-        return glue.join(map(str, self._items))
 
-    def len(self):
+        def inner():
+            while True:
+                result = self.take(n).list()
+                if not result:
+                    return
+                yield Slinkie(result)
+
+        return Slinkie(inner())
+
+    def skip(self, n):
         """
-        Consumes all items to produce a count.
+        Skip n items.
         """
-        return sum(1 for _ in self._items)
+        for i in range(n):
+            next(self._items)
+        return self
+
+    def sort(self, key=None, reverse=False):
+        """
+        Sorts the items by key.
+        """
+        return Slinkie(sorted(self._items, key=key, reverse=reverse))
+
+    def take(self, n):
+        """
+        Take n items.
+        """
+
+        def inner():
+            try:
+                for _ in range(n):
+                    yield next(self._items)
+            except StopIteration:
+                return
+
+        return Slinkie(inner())
 
     def transpose(self):
         """
@@ -218,23 +206,15 @@ class Slinkie:
         """
         return Slinkie(zip(*self._items))
 
-    def list(self):
-        """
-        Returns a list of all items.
-        """
-        return list(self._items)
+    # Functions consuming the slinkie.
 
-    def tuple(self):
+    def len(self):
         """
-        Returns a tuple of all items.
+        Consumes all items to produce a count.
         """
-        return tuple(self._items)
+        return sum(1 for _ in self._items)
 
-    def set(self):
-        """
-        Returns a set of all items.
-        """
-        return set(self._items)
+    # Functions transforming the Slinkie to another type of collection.
 
     def dict(self, key=None, transform=None):
         """
@@ -249,10 +229,33 @@ class Slinkie:
 
         return {key(it): transform(it) for it in self._items}
 
-    # Aliases
+    def list(self):
+        """
+        Returns a list of all items.
+        """
+        return list(self._items)
+
+    def set(self):
+        """
+        Returns a set of all items.
+        """
+        return set(self._items)
+
+    def str(self, glue=''):
+        """
+        Joins the items by glue, where glue is a string. Calls glue.join.
+        """
+        return glue.join(map(str, self._items))
+
+    def tuple(self):
+        """
+        Returns a tuple of all items.
+        """
+        return tuple(self._items)
+
+    # Aliases.
+
     where = filter
     select = map
     count = len
-    __add__ = extend
-    __sub__ = exclude
-    __rshift__ = skip
+    join = str
